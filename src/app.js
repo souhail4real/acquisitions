9616c04b6f0b6fbd61bdec3751f265b1db93db44
souhail4real <souhailaboumida1@gmail.com>
2026-01-07 17:25:41 +0100

    implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
-    response.status(200).send('Hello from acquisitions!');
+  response.status(200).send('Hello from acquisitions!');
 });
 
 export default app;
