cde76f54d05d9ec52ec9a6fbd5df1dfec225b2be
souhail4real <souhailaboumida1@gmail.com>
2026-01-07 16:05:35 +0100

    initial commt

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+    response.status(200).send('Hello from acquisitions!');
+});
+
+export default app;
